f90ecb72cf63685a78b342de92b62a3a99a21bc8
Madeline <46743919+MaddyUnderStars@users.noreply.github.com>
2023-02-02 20:30:57 +1100

    Missing routes search

---
+++ docs/assets/js/missingroutes.js
@@ -0,0 +1,27 @@
+const MISSING_ROUTES_LIST = "https://raw.githubusercontent.com/fosscord/fosscord-missing-routes/main/missing.json";
+const listMount = document.getElementById("missing-routes-list");
+
+document.getElementById("missing-routes-search").addEventListener("input", (e) => {
+	const content = e.target.value.toLowerCase();
+
+	for (let elem of listMount.children) {
+		if (elem.innerText.toLowerCase().indexOf(content) !== -1) 
+			elem.style.display = "list-item";
+		else
+			elem.style.display = "none";
+	}
+});
+
+(async () => {
+	const res = await fetch(MISSING_ROUTES_LIST);
+	const missingRoutes = await res.json();
+
+	for (let route of missingRoutes) {
+		const elem = document.createElement("li");
+		const inner = document.createElement("code")
+		inner.innerText = route;
+		elem.appendChild(inner);
+
+		listMount.appendChild(elem);
+	}
+})();
