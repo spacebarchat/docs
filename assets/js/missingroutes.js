801103224cdd10dd60af1836dcd5ae5f30255279
MaddyUnderStars <46743919+MaddyUnderStars@users.noreply.github.com>
2023-02-03 02:24:45 +0000

    Deploying to gh-pages from @ fosscord/fosscord-docs@291e6f2d07f305c0ab016f8799dfffe661133b6d 🚀

--- assets/js/missingroutes.js
+++ assets/js/missingroutes.js
@@ -1,16 +1,18 @@
-const MISSING_ROUTES_LIST = "https://raw.githubusercontent.com/fosscord/fosscord-missing-routes/main/missing.json";
+const MISSING_ROUTES_LIST =
+	"https://raw.githubusercontent.com/fosscord/fosscord-missing-routes/main/missing.json";
 const listMount = document.getElementById("missing-routes-list");
 
-document.getElementById("missing-routes-search").addEventListener("input", (e) => {
-	const content = e.target.value.toLowerCase();
+document
+	.getElementById("missing-routes-search")
+	.addEventListener("input", (e) => {
+		const content = e.target.value.toLowerCase();
 
-	for (let elem of listMount.children) {
-		if (elem.innerText.toLowerCase().indexOf(content) !== -1) 
-			elem.style.display = "list-item";
-		else
-			elem.style.display = "none";
-	}
-});
+		for (let elem of listMount.children) {
+			if (elem.innerText.toLowerCase().indexOf(content) !== -1)
+				elem.style.display = "list-item";
+			else elem.style.display = "none";
+		}
+	});
 
 (async () => {
 	const res = await fetch(MISSING_ROUTES_LIST);
@@ -18,7 +20,7 @@ document.getElementById("missing-routes-search").addEventListener("input", (e) =
 
 	for (let route of missingRoutes) {
 		const elem = document.createElement("li");
-		const inner = document.createElement("code")
+		const inner = document.createElement("code");
 		inner.innerText = route;
 		elem.appendChild(inner);
 
