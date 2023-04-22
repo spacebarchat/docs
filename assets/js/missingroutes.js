a443d6f956fa8cfa54ac7d9298eaae6344f74637
MaddyUnderStars <46743919+MaddyUnderStars@users.noreply.github.com>
2023-04-22 08:19:10 +0000

    Deploying to gh-pages from @ spacebarchat/docs@78c376c30f69a724b0c3fa7f00877db364e2c370 🚀

--- assets/js/missingroutes.js
+++ assets/js/missingroutes.js
@@ -16,7 +16,11 @@ document
 
 (async () => {
 	const res = await fetch(MISSING_ROUTES_LIST);
-	const missingRoutes = await res.json();
+	const json = await res.json();
+	const missingRoutes = json.routes;
+
+	document.getElementById("counter").innerHTML = `We implement ${json.missing}/${json.discord} endpoints from Discord.com ` +
+		`as well as ${(json.spacebar + json.missing) - json.discord} additional endpoints.`;
 
 	for (let route of missingRoutes) {
 		const elem = document.createElement("li");
